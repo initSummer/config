8d0c1d39e888bc94bd99c45b313ff20a1eefb0de
summer <summer_test@outlook.com>
2023-11-06 08:11:47 +0800

    update

--- commands/alias.sh
+++ commands/alias.sh
@@ -1,4 +1,3 @@
-alias autopush='sh ~/MyConfig/autoPush.sh '
 alias gita="git add -u"
 alias gitc="git commit -m"
 alias gits="git status"
@@ -8,7 +7,7 @@ alias ll='ls -l'
 alias p='python3'
 alias python='python3'
 alias q='exit'
-alias rename='rename.ul'
+#alias rename='rename.ul'
 alias vi='vim'
 
 # source
