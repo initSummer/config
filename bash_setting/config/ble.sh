1109dbdf12d9979b8b4d2d9b7d5b4e5d2b783b93
summer <summer@example.com>
2026-09-03 16:55:06 +0800

    fix: ble

--- bash_setting/config/ble.sh
+++ bash_setting/config/ble.sh
@@ -3,6 +3,3 @@ ble-face auto_complete='fg=240,underline,italic'
 ble-face command_builtin='fg=86'          # 内建命令
 ble-face command_function='fg=108,bold'   # 函数调用
 
-ble-face menu_complete_match='fg=red,bold'
-ble-face menu_complete_selected='reverse'
-
