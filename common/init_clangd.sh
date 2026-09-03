5b67f7708113b1bf725341219525195d36af007a
summer <summer@example.com>
2026-09-03 09:17:59 +0800

    refactor: opt

--- common/init_clangd.sh
+++ common/init_clangd.sh
@@ -1,14 +1,18 @@
-clangd_name="clangd_22.1.6"
-clangd_tar_name="${clangd_name}.tar.gz"
-clangd_home="${SUMMER_CONFIG_DIR}/app"
-clangd_path=${clangd_home}/${clangd_name}
-clangd_tar_path=${clangd_home}/${clangd_tar_name}
+init_app() {
+local app_name=$1
+local app_tar_name="${app_name}.tar.gz"
+local app_home="${SUMMER_CONFIG_DIR}/app"
+local app_path=${app_home}/${app_name}
+local app_tar_path=${app_home}/${app_tar_name}
 
-if [ ! -f ${clangd_tar_path} ]; then
-    cat ${clangd_tar_path}.part_* > ${clangd_tar_path}
+if [ ! -f ${app_tar_path} ]; then
+    cat ${app_tar_path}.part_* > ${app_tar_path}
 fi
 
-if [ ! -d ${clangd_path} ]; then
-    tar -zxf ${clangd_tar_path} -C ${clangd_home}
+if [ ! -d ${app_path} ]; then
+    tar -zxf ${app_tar_path} -C ${app_home}
 fi
+}
 
+
+init_app "clangd_22.1.6"
